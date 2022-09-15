840c750e7947f5ddf943c156bf869213c53ae91c
Mahmudul Hasan <sohel.official@gmail.com>
2022-09-15 11:51:53 +0600

    Update MemoryTracking.cpp

--- Memory/MemoryTracking/MemoryTracking/MemoryTracking.cpp
+++ Memory/MemoryTracking/MemoryTracking/MemoryTracking.cpp
@@ -2,6 +2,8 @@
 
 void* operator new(size_t size)
 {
+	std::cout << size << std::endl;
+
 	return malloc(size);
 }
 
@@ -15,4 +17,6 @@ int main()
 	MyClass* obj = new MyClass();
 	std::unique_ptr<MyClass> obj1 = std::make_unique<MyClass>();
 	std::string name = "name";
+	int* value = new int(10);
+	std::cout << name << std::endl;
 }
